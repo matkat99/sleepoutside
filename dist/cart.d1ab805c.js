cf4482911caca3efee960ccd2b54065a0d39d5c4
matkat99 <st83440@gmail.com>
2020-11-16 09:50:36 -0700

    remove docs

--- dist/cart.d1ab805c.js
+++ dist/cart.d1ab805c.js
@@ -1,4 +1,97 @@
-parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"zaPY":[function(require,module,exports) {
-function a(a){return JSON.parse(localStorage.getItem(a))}function c(){var c=a("so-cart").map(function(a){return n(a)});document.querySelector(".product-list").innerHTML=c.join("")}function n(a){var c='<li class="cart-card divider">\n  <a href="#" class="cart-card__image">\n    <img\n      src="'.concat(a.Image,'"\n      alt="').concat(a.Name,'"\n    />\n  </a>\n  <a href="#">\n    <h2 class="card__name">').concat(a.Name,'</h2>\n  </a>\n  <p class="cart-card__color">').concat(a.Colors[0].ColorName,'</p>\n  <p class="cart-card__quantity">qty: 1</p>\n  <p class="cart-card__price">$').concat(a.FinalPrice,"</p>\n</li>");return console.log(c),c}c();
-},{}]},{},["zaPY"], null)
+parcelRequire = (function (e, r, t, n) {
+  var i,
+    o = "function" == typeof parcelRequire && parcelRequire,
+    u = "function" == typeof require && require;
+  function f(t, n) {
+    if (!r[t]) {
+      if (!e[t]) {
+        var i = "function" == typeof parcelRequire && parcelRequire;
+        if (!n && i) return i(t, !0);
+        if (o) return o(t, !0);
+        if (u && "string" == typeof t) return u(t);
+        var c = new Error("Cannot find module '" + t + "'");
+        throw ((c.code = "MODULE_NOT_FOUND"), c);
+      }
+      (p.resolve = function (r) {
+        return e[t][1][r] || r;
+      }),
+        (p.cache = {});
+      var l = (r[t] = new f.Module(t));
+      e[t][0].call(l.exports, p, l, l.exports, this);
+    }
+    return r[t].exports;
+    function p(e) {
+      return f(p.resolve(e));
+    }
+  }
+  (f.isParcelRequire = !0),
+    (f.Module = function (e) {
+      (this.id = e), (this.bundle = f), (this.exports = {});
+    }),
+    (f.modules = e),
+    (f.cache = r),
+    (f.parent = o),
+    (f.register = function (r, t) {
+      e[r] = [
+        function (e, r) {
+          r.exports = t;
+        },
+        {},
+      ];
+    });
+  for (var c = 0; c < t.length; c++)
+    try {
+      f(t[c]);
+    } catch (e) {
+      i || (i = e);
+    }
+  if (t.length) {
+    var l = f(t[t.length - 1]);
+    "object" == typeof exports && "undefined" != typeof module
+      ? (module.exports = l)
+      : "function" == typeof define && define.amd
+      ? define(function () {
+          return l;
+        })
+      : n && (this[n] = l);
+  }
+  if (((parcelRequire = f), i)) throw i;
+  return f;
+})(
+  {
+    zaPY: [
+      function (require, module, exports) {
+        function a(a) {
+          return JSON.parse(localStorage.getItem(a));
+        }
+        function c() {
+          var c = a("so-cart").map(function (a) {
+            return n(a);
+          });
+          document.querySelector(".product-list").innerHTML = c.join("");
+        }
+        function n(a) {
+          var c = '<li class="cart-card divider">\n  <a href="#" class="cart-card__image">\n    <img\n      src="'
+            .concat(a.Image, '"\n      alt="')
+            .concat(
+              a.Name,
+              '"\n    />\n  </a>\n  <a href="#">\n    <h2 class="card__name">'
+            )
+            .concat(a.Name, '</h2>\n  </a>\n  <p class="cart-card__color">')
+            .concat(
+              a.Colors[0].ColorName,
+              '</p>\n  <p class="cart-card__quantity">qty: 1</p>\n  <p class="cart-card__price">$'
+            )
+            .concat(a.FinalPrice, "</p>\n</li>");
+          return console.log(c), c;
+        }
+        c();
+      },
+      {},
+    ],
+  },
+  {},
+  ["zaPY"],
+  null
+);
 //# sourceMappingURL=/sleepoutside/dist/cart.d1ab805c.js.map
