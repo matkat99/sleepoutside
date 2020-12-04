0a6629e6a760a8db5cac83c9dc5601a58db57824
matkat99 <st83440@gmail.com>
2020-12-04 15:59:28 -0700

    add build

---
+++ build/js/cart.js
@@ -0,0 +1,14 @@
+function getLocalStorage(a){return JSON.parse(localStorage.getItem(a))}function getCartContents(){let a="";const r=getLocalStorage("so-cart"),c=r.map(t=>renderCartItem(t));document.querySelector(".product-list").innerHTML=c.join("")}function renderCartItem(a){const r=`<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${a.Image}"
+      alt="${a.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${a.Name}</h2>
+  </a>
+  <p class="cart-card__color">${a.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${a.FinalPrice}</p>
+</li>`;return console.log(r),r}getCartContents();
---
+++ build/js/utils.js
@@ -0,0 +1,1 @@
+export function qs(e){return document.querySelector(e)}export function getLocalStorage(e){return JSON.parse(localStorage.getItem(e))}export function setLocalStorage(e,t){localStorage.setItem(e,JSON.stringify(t))}export function setClick(e,t){qs(e).addEventListener("touchend",n=>{n.preventDefault(),t()}),qs(e).addEventListener("click",t)}
