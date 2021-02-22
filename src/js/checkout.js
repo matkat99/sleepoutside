0f93835fe077adc1b98995bf115d37055e1ba060
matkat99 <st83440@gmail.com>
2021-02-22 13:51:04 -0700

    add submit case to checkout

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -7,6 +7,7 @@ const myCheckout = new CheckoutProcess('so-cart', '.checkout-summary');
 myCheckout.init();
 
 document.querySelector('#zip').addEventListener('blur', myCheckout.calculateOrdertotal.bind(myCheckout));
+// this is how it would look if we listen for the click on the button
 document.querySelector('#checkoutSubmit')
 .addEventListener('click', (e) => {
   e.preventDefault();
@@ -16,3 +17,12 @@ document.querySelector('#checkoutSubmit')
   if(chk_status) 
     myCheckout.checkout();
 });
+
+
+// this is how it would look if we listen for the submit on the form
+// document.forms['checkout']
+// .addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   // e.target would contain our form in this case
+//    myCheckout.checkout();
+// });
