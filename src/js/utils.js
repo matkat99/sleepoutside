ce8dc8529b3e1478104b1b57a878856a70df4cb1
matkat99 <st83440@gmail.com>
2021-05-11 12:51:09 -0600

    update qs

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,7 +1,9 @@
 // wrapper for querySelector...returns matching element
-export function qs(selector) {
-  return document.querySelector(selector);
+export function qs(selector, parent = document) {
+  return parent.querySelector(selector);
 }
+// or a more concise version if you are into that sort of thing:
+// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
