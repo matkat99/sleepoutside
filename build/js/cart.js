083c0d5665af6136885c6edb7cf37f0dad3516c0
byui-programmer-96 <bam21002@byui.edu>
2022-11-04 18:33:18 -0500

    week 2 commit

--- build/js/cart.js
+++ build/js/cart.js
@@ -1,4 +1,14 @@
-function getLocalStorage(a){return JSON.parse(localStorage.getItem(a))}function getCartContents(){let a="";const r=getLocalStorage("so-cart"),c=r.map(t=>renderCartItem(t));document.querySelector(".product-list").innerHTML=c.join("")}function renderCartItem(a){const r=`<li class="cart-card divider">
+function getLocalStorage(a) {
+  return JSON.parse(localStorage.getItem(a));
+}
+function getCartContents() {
+  let a = "";
+  const r = getLocalStorage("so-cart"),
+    c = r.map((t) => renderCartItem(t));
+  document.querySelector(".product-list").innerHTML = c.join("");
+}
+function renderCartItem(a) {
+  const r = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
       src="${a.Image}"
@@ -11,4 +21,7 @@ function getLocalStorage(a){return JSON.parse(localStorage.getItem(a))}function 
   <p class="cart-card__color">${a.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${a.FinalPrice}</p>
-</li>`;return console.log(r),r}getCartContents();
+</li>`;
+  return console.log(r), r;
+}
+getCartContents();
--- build/js/utils.js
+++ build/js/utils.js
@@ -1,1 +1,15 @@
-export function qs(e,t=document){return t.querySelector(e)}export function getLocalStorage(e){return JSON.parse(localStorage.getItem(e))}export function setLocalStorage(e,t){localStorage.setItem(e,JSON.stringify(t))}export function setClick(e,t){qs(e).addEventListener("touchend",n=>{n.preventDefault(),t()}),qs(e).addEventListener("click",t)}
+export function qs(e, t = document) {
+  return t.querySelector(e);
+}
+export function getLocalStorage(e) {
+  return JSON.parse(localStorage.getItem(e));
+}
+export function setLocalStorage(e, t) {
+  localStorage.setItem(e, JSON.stringify(t));
+}
+export function setClick(e, t) {
+  qs(e).addEventListener("touchend", (n) => {
+    n.preventDefault(), t();
+  }),
+    qs(e).addEventListener("click", t);
+}
