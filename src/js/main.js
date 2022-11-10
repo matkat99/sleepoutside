2a5f8699dfc379117376808ef72c2bc9f29ac5d2
Shane Thompson <st83440@gmail.com>
2022-11-10 15:01:29 -0700

    team5 vite initial

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,3 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
 
 loadHeaderFooter();
-
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductList("Tents", dataSource, element);
-
-listing.init();
