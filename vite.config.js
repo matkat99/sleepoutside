5934cd54fbcb103c6015f3f8a9a47cfd2a32e860
Shane Thompson <st83440@gmail.com>
2022-11-11 16:06:44 -0700

    move checkout

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/cart/checkout.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
         listing: resolve(__dirname, "src/product-listing/index.html"),
       },
