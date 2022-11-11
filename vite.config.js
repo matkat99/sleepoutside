198afaf367e2e17d25b6e8b28c9de85ef6b84c37
Shane Thompson <st83440@gmail.com>
2022-11-11 11:51:16 -0700

    add new input to config file

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
+        listing: resolve(__dirname, "src/product-listing/index.html"),
       },
     },
   },
