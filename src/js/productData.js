307b7d237bcf7d69d7d4153fdc8ca6c554b4ea51
Ryan Alvord <90877920+Luckysandwich7@users.noreply.github.com>
2022-11-17 19:11:00 -0700

    Full L06 Team Activity

---
+++ src/js/productData.js
@@ -0,0 +1,29 @@
+const baseURL = 'http://server-nodejs.cit.byui.edu:3000/'
+function convertToJson(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error('Bad Response');
+  }
+}
+
+export default class ProductData  {
+  constructor() {
+    //Using the API means we don't need to tie the dataSource to a specific category anymore. So we can remove this in the constructor.
+    // this.category = category;
+    // this.path = `../json/${this.category}.json`;
+   
+  }
+  getData(category) {
+    // instead we will pass the category we want in here when we need it.
+    return fetch(baseURL + `products/search/${category}`)
+    .then(convertToJson).then((data) => data.Result);
+  }
+  async findProductById(id) {
+    //const products = await this.getData()
+    //return products.find((item) => item.Id === id);
+    // the API allows us to pull products directly from it by ID...so we can change this method as well to take advantage of that.
+    return await fetch(baseURL + `product/${id}`).then(convertToJson)
+      .then((data) => data.Result);
+  }
+}
