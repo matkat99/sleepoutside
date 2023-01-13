393815f69e91d33e1c5d6459e33bd2576edbb113
FelixS2000 <guz22002@byui.edu>
2023-01-13 19:55:16 -0400

    product's change

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,12 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails  from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = setLocalStorage("product");
 
+const products = new ProductDetails(productId, dataSource);
+products.init();
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
 }
