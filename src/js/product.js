66d393a54701ffd8ecf9c136d2e5aabc6d58ed85
FelixS2000 <guz22002@byui.edu>
2023-01-14 08:08:14 -0400

    save product

--- src/js/product.js
+++ src/js/product.js
@@ -1,33 +1,39 @@
-import { getParam } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails  from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam("product");
 
-const products = new ProductDetails(productId, dataSource);
-products.init();
 function addProductToCart(product) {
-  getParam("so-cart", product);
+  let cart = getLocalStorage("soCart");
+  if (cart) {
+    let tent = 1;
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i].Id == product.Id) {
+        product.quantity = cart[i].quantity++;
+        tent = 0;
+      }
+    }
+
+    if (tent == 1) {
+      product.quantity = 1;
+      cart.push(product);
+    }
+  } else {
+    cart = [];
+    product.quantity = 1;
+    cart.push(product);
+  }
+
+  setLocalStorage("soCart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
-    // Get the button element by its ID
-  var button = document.getElementById("addToCart");
-
-  // Add an event listener to the button
-  button.addEventListener("click", function() {
-      // This function will be called when the button is clicked
-      alert("You purchase!")
-      console.log(product["Id"])
-    });
-    
 }
 
 // add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
-  
