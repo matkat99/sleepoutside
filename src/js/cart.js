c0ceefb0291e9a229efab90a7ec7abef4118d773
FelixS2000 <guz22002@byui.edu>
2023-01-14 20:30:12 -0400

    init commit

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,7 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("soCart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
@@ -18,7 +18,7 @@ function cartItemTemplate(item) {
     <h2 class='card__name'>${item.Name}</h2>
   </a>
   <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
-  <p class='cart-card__quantity'>qty: 1</p>
+  <p class='cart-card__quantity'> ${item.quantity}</p>
   <p class='cart-card__price'>$${item.FinalPrice}</p>
 </li>`;
 
