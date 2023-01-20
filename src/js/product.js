7114be509aeaa091b1ea6b4617464f9f2543038b
Zeir Braidi <zeirbraidi@gmail.com>
2023-01-20 10:58:48 -0700

    Revert "Merge pull request #7 from JosueBCe/jc--team2"
    This reverts commit dd93199285c1919b2f0f767018ba9756aa27cdf3, reversing
    changes made to 4b0e5c22b83f05456aab1121ead9f210defb93d8.

--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,7 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from './ProductDetails.mjs';
 
-const productId = getParam('product');
-const dataSource = new ProductData('tents');
-
-const product = new ProductDetails(productId, dataSource);
-product.init();
-
-console.log(dataSource.findProductById(productId));
+const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
   let cart = getLocalStorage("soCart");
