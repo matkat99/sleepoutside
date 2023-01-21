5c8cc7f4740b04cdbe27c544a9ce9d466ac9a5a2
diannaqui <qui22001@byui.edu>
2023-01-21 15:19:40 -0700

    product_pages added

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,7 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("soCart") || [];
+  const cartItems = getLocalStorage("so-cart") || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
@@ -18,11 +18,9 @@ function cartItemTemplate(item) {
     <h2 class='card__name'>${item.Name}</h2>
   </a>
   <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
-  <p class='cart-card__quantity'>qty: ${item.quantity}</p>
+  <p class='cart-card__quantity'>qty: </p>
   <p class='cart-card__price'>$${item.FinalPrice}</p>
 </li>`;
-
   return newItem;
 }
-
 renderCartContents();
