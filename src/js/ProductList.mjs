ce28b48b95cfe3219fa1e2f63ddfbab8e9dd315f
Felix <fguzmanceri@gmail.com>
2023-01-24 13:42:58 -0400

    product list update

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -4,12 +4,12 @@ function productCardTemplate(product) {
   return `<li class="product-card">
   <a href="product_pages/index.html?product=">
   <img
-    src=""
-    alt="Image of "
+    src="${product.Image}"
+    alt="Image of ${product.Name}"
   />
-  <h3 class="card__brand"></h3>
-  <h2 class="card__name"></h2>
-  <p class="product-card__price">$</p></a>
+  <h3 class="card__brand">${product.Brand.Name}</h3>
+  <h2 class="card__name">${product.Name}</h2>
+  <p class="product-card__price">$${product.FinalPrice}</p></a>
 </li>`
 }
 export default class ProductList{
