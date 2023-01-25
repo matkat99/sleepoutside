9fb11efe8cd5f20c8193d366cea21972a1efbdb6
Felix <fguzmanceri@gmail.com>
2023-01-25 12:44:21 -0400

    changing

--- src/js/product.js
+++ src/js/product.js
@@ -7,27 +7,3 @@ const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-
-// add to cart button event handler
-//async function addToCartHandler(e) {
-  //const product = await dataSource.findProductById(e.target.dataset.id);
-  //addProductToCart(product);
-  // Get the button element by its ID
-//var button = document.getElementById("addToCart");
-
-// Add an event listener to the button
-//button.addEventListener("click", function() {
-    // This function will be called when the button is clicked
-    //alert("You purchase!" + "" + product["Name"])
-    
-  //});
-  
-//}
-
-// add listener to Add to Cart button
-//document
-  //.getElementById("addToCart")
-  //.addEventListener("click", addToCartHandler);
-
-  
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -13,6 +13,32 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
+// helper to get parameter strings
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
+// function to take a list of objects and a template and insert the objects as HTML into the DOM
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
@@ -20,11 +46,4 @@ export function setClick(selector, callback) {
     callback();
   });
   qs(selector).addEventListener("click", callback);
-}
-// helper to get parameter strings
-export function getParam(param) {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get(param);
-  return product;
 }
