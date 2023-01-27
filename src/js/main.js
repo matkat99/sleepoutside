fa343074e98cd83e0d07f63050e9b06ca6a7f533
Caleb Barzee <barzeec@gmail.com>
2023-01-27 12:15:29 -0700

    New vite config

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-import Cart from './Cart.mjs';
-import { getParam } from './utils.mjs';
-import ProductData from './ProductData.mjs';
-import ProductDetails from './ProductDetails.mjs';
+import Cart from "./Cart.mjs";
+import { getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData('tents');
-const productId = getParam('product');
+const dataSource = new ProductData("tents");
+const productId = getParam("product");
 let shoppingCart = new Cart();
 
 const product = new ProductDetails(productId, dataSource);
