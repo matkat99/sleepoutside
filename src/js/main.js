ce066a7809aa11d0e27298480e35102448c9f7d8
Felix <fguzmanceri@gmail.com>
2023-01-28 10:32:10 -0400

    some changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-import ProductListing from "./ProductList.mjs"
 import ProductData from "./ProductData.mjs";
+import ProductListing from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents"); 
 const element = document.querySelector(".product-list");
