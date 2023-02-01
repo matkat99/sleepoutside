389fd1a67e136ddcb85bc8d9ef7053699caf0e60
Felix <fguzmanceri@gmail.com>
2023-02-01 12:27:45 -0400

    updates

---
+++ src/js/checkout.js
@@ -0,0 +1,3 @@
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
+import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
+
+loadHeaderFooter();
 
 const dataSource = new ProductData("tents"); 
 const element = document.querySelector(".product-list");
