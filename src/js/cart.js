45570233f3087e1615d3e856c40051397d641540
Felix <fguzmanceri@gmail.com>
2023-02-01 15:44:37 -0400

    changes

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,6 +2,6 @@ import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
-
+getLocalStorage("so-cart", this.addToCart);
 const cart = new ShoppingCart("so-cart", ".product-list");
 cart.renderCartContents();
--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,8 @@
-import { getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
+
+loadHeaderFooter();
 
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
