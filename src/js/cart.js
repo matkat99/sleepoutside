21377926e343c627cdcb5b3721be0873189c58c6
Felix <fguzmanceri@gmail.com>
2023-02-02 00:20:05 -0400

    cart's update 2

--- src/js/cart.js
+++ src/js/cart.js
@@ -39,7 +39,34 @@ function sumTotal(cart) {
   return total;
 }
 
+function updateCartCount() {
+  let count = getLocalStorage("so-cart") || 0;
+  count = parseInt(count, 10);
+  
+  const cartCountElement = document.querySelector(".product-list");
+  cartCountElement.textContent = count;
+  
+  if (count === 0) {
+    cartCountElement.style.display = "none";
+  } else {
+    cartCountElement.style.display = "block";
+  }
+}
 
+// Call the updateCartCount function when the page loads
+window.addEventListener("load", updateCartCount);
 
+// Update the cart count in local storage whenever it changes
+function addToCart() {
+  let count = getLocalStorage("so-cart") || 0;
+  count = parseInt(count, 10);
+  count += 1;
+  
+  getLocalStorage.setItem(".product-list", count);
+  updateCartCount();
+}
+
+updateCartCount();
+addToCart();
 renderCartContents();
 loadHeaderFooter();
