6fd32492bbf53706ec3f089d5a7ea9ac6f6a8ada
Felix <fguzmanceri@gmail.com>
2023-02-02 00:50:53 -0400

    changes

--- src/js/superscriptNumber.js
+++ src/js/superscriptNumber.js
@@ -1,26 +1,4 @@
-function updateCartCount() {
-  let count = localStorage.getItem("cartCount") || 0;
-  count = parseInt(count, 10);
-  
-  const cartCountElement = document.querySelector(".cart-count");
-  cartCountElement.textContent = count;
-  
-  if (count === 0) {
-    cartCountElement.style.display = "none";
-  } else {
-    cartCountElement.style.display = "block";
-  }
-}
-
-// Call the updateCartCount function when the page loads
-window.addEventListener("load", updateCartCount);
-
-// Update the cart count in local storage whenever it changes
-function addToCart() {
-  let count = localStorage.getItem("cartCount") || 0;
-  count = parseInt(count, 10);
-  count += 1;
-  
-  localStorage.setItem("cartCount", count);
-  updateCartCount();
-}
+document.getElementById("addToCart").addEventListener("click", function() {
+  let count = parseInt(document.getElementById("cart-count").innerHTML) + 1;
+  document.getElementById("cart-count").innerHTML = count;
+});
