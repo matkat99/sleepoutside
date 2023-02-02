501d3c63fd8674803beec16d7d861a8b2393833b
Felix <fguzmanceri@gmail.com>
2023-02-02 11:20:28 -0400

    colors

--- src/js/cart.js
+++ src/js/cart.js
@@ -39,6 +39,20 @@ function sumTotal(cart) {
   return total;
 }
 
+// Get cart count from local storage
+let cartCount = localStorage.getItem("cartCount") || 0;
 
+// Update the cart count display
+document.getElementById("cartCount").innerText = cartCount;
+
+// Update the cart count in local storage when adding items to cart
+function addToCart() {
+  cartCount++;
+  localStorage.setItem("cartCount", cartCount);
+  document.getElementsByName("so-cart").innerText = cartCount;
+}
+
+addToCart();
 loadHeaderFooter();
 renderCartContents();
+
