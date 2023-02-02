13c51ed47ebb3ce1c5fc562f4f64d08da0c8a245
Felix <fguzmanceri@gmail.com>
2023-02-02 14:08:02 -0400

    saving

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,13 @@ import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 import {addToCartBtn,cartQuantitySpan} from "./superscriptNumber";
 
-cartQuantitySpan();
-addToCartBtn();
+
 loadHeaderFooter();
 
+const cartQuantity = new cartQuantitySpan("cart-quantity");
+const cartBtn = new addToCartBtn("addToCart");
+const itemQuantity = ProductData(cartQuantity,cartBtn);
+itemQuantity.init();
 const dataSource = new ProductData("tents"); 
 const element = document.querySelector(".product-list");
 const listing = new ProductListing("Tents", dataSource, element);
