d2982be183aebb8c8498b8fbeb508b830e6d2251
Felix <fguzmanceri@gmail.com>
2023-02-02 14:12:52 -0400

    cart changes

--- src/js/cart.js
+++ src/js/cart.js
@@ -39,20 +39,7 @@ function sumTotal(cart) {
   return total;
 }
 
-// Get cart count from local storage
-let cartCount = localStorage.getItem("cartCount") || 0;
 
-// Update the cart count display
-document.getElementById("cartCount").innerText = cartCount;
-
-// Update the cart count in local storage when adding items to cart
-function addToCart() {
-  cartCount++;
-  localStorage.setItem("cartCount", cartCount);
-  document.getElementsByName("so-cart").innerText = cartCount;
-}
-
-addToCart();
 loadHeaderFooter();
 renderCartContents();
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,11 @@
 import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
-import {cartQuantitySpan} from "./superscriptNumber";
 
 
 loadHeaderFooter();
 
-const cartQuantity = new cartQuantitySpan("cart-quantity");
-const itemQuantity = new cartQuantity(cartQuantity);
-itemQuantity.init();
+
 const dataSource = new ProductData("tents"); 
 const element = document.querySelector(".product-list");
 const listing = new ProductListing("Tents", dataSource, element);
