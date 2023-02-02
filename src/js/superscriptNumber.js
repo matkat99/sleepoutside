0df88db4a0aa41d7bdb4cef0883d58ac86a42055
Felix <fguzmanceri@gmail.com>
2023-02-02 01:06:24 -0400

    changes

--- src/js/superscriptNumber.js
+++ src/js/superscriptNumber.js
@@ -1,4 +1,13 @@
-document.querySelector("#addToCart").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function() {
+  let count = localStorage.getItem("cartCount");
+  if (count === null) {
+    count = 0;
+  }
+  document.getElementById("cart-count").innerHTML = count;
+});
+
+document.getElementById("add-to-cart").addEventListener("click", function() {
   let count = parseInt(document.getElementById("cart-count").innerHTML) + 1;
-  document.getElementById("data-id").innerHTML = count;
+  document.getElementById("cart-count").innerHTML = count;
+  localStorage.setItem("cartCount", count);
 });
