00001363ee78ceadb5bc12cab9f60b7116aa5e0b
Felix <fguzmanceri@gmail.com>
2023-02-03 16:58:01 -0400

    changes 1

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -1,32 +1,51 @@
-import { getLocalStorage }  from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
-  return newItem;
+    const newItem = `<li class='cart-card divider'>
+    <a href='#' class='cart-card__image'>
+      <img
+        src='${item.Image}'
+        alt='${item.Name}'
+      />
+    </a>
+    <a href='#'>
+      <h2 class='card__name'>${item.Name}</h2>
+    </a>
+    <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
+    <p class='cart-card__quantity'>qty: ${item.quantity}</p>
+    <p class='cart-card__price'>$${item.FinalPrice}</p>
+  </li>`;
+    return newItem;
 }
 
-export default class ShoppingCart{
-  constructor(key, parentSelector){
-    this.key = key;
-    this.parentSelector = parentSelector;
-  }
-  renderCartContents() {
-    const cartItems = getLocalStorage(this.key);
-    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
-  }
+export default class ShoppingCart {
+    constructor(key, parentSelector) {
+      this.key = key;
+      this.parentSelector = parentSelector;
+    }
+    // renderCartContents() {
+    //   const cartItems = getLocalStorage(this.key);
+    //   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    //   document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
+    // }
+    renderCartContents() {
+      const cartItems = getLocalStorage(this.key) || [];
+      let cartTotal = document.querySelector(".cart-total")
+
+      /* If there's something in the Cart, display the items and the total sum of them. */
+      if (cartItems.length != 0) {
+        console.log(cartItems)
+        const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+        document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
+
+        cartTotal.style.display = "block"; // Make appear the total paragraph that is hidden by default
+        cartTotal.innerHTML = `Total: ${sumTotal(cartItems).toFixed(2)}`
+      }
+    }
+}
+
+function sumTotal(cart) {
+  let total = 0;
+  cart.forEach(item => total += (item.FinalPrice * item.quantity));
+  return total;
 }
--- src/js/cart.js
+++ src/js/cart.js
@@ -1,13 +1,8 @@
 import { loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
 
-function sumTotal(carts) {
-  let total = 0;
-  carts.forEach(item => total += (item.FinalPrice * item.quantity));
-  return total;
-}
-
-sumTotal();
 loadHeaderFooter();
-const cart =  new ShoppingCart("so-cart",".product-list");
-cart.renderCartContents();
+
+const ShoppingBag =  new ShoppingCart("so-cart",".product-list");
+
+ShoppingBag.renderCartContents();
