00cbd4ab8c4554c44685b7c6d49d988b152a313e
Felix <fguzmanceri@gmail.com>
2023-02-03 11:59:23 -0400

    cart html

--- src/js/superscriptNumber.js
+++ src/js/superscriptNumber.js
@@ -1,13 +1,26 @@
 const addToCartButton = document.getElementById("addToCart");
+const removeFromCartButton = document.getElementById("removeFromCart");
 const cartQuantitySpan = document.querySelector(".cart-quantity");
+
+function updateCartCount(count) {
+  localStorage.setItem("itemCount", count);
+  cartQuantitySpan.innerHTML = count + (count === 1 ? "<sup>st</sup>" : (count === 2 ? "<sup>nd</sup>" : (count === 3 ? "<sup>rd</sup>" : "<sup>th</sup>")));
+}
 
 addToCartButton.addEventListener("click", () => {
   let currentCount = Number(localStorage.getItem("itemCount")) || 0;
   currentCount++;
-  localStorage.setItem("itemCount", currentCount);
-  cartQuantitySpan.innerText = currentCount;
+  updateCartCount(currentCount);
+});
+
+removeFromCartButton.addEventListener("click", () => {
+  let currentCount = Number(localStorage.getItem("itemCount")) || 0;
+  if (currentCount > 0) {
+    currentCount--;
+    updateCartCount(currentCount);
+  }
 });
 
 // On page load, get the item count from local storage and update the span
 const itemCount = Number(localStorage.getItem("itemCount")) || 0;
-cartQuantitySpan.innerText = itemCount;
+updateCartCount(itemCount);
