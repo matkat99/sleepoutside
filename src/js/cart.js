e70db0ca0604ac185acbccae2484692d1c2a4462
Felix <fguzmanceri@gmail.com>
2023-02-04 00:42:13 -0400

    yolo

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,6 +3,6 @@ import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
 
-const ShoppingBag =  new ShoppingCart("so-cart",".product-list");
+const ShoppingBag =  new ShoppingCart("so-cart", ".product-list");
 
 ShoppingBag.renderCartContents();
