bd8214d6aa5d635a806cdcef3425b31f1600ef6c
josuecenturion1@gmail.com <josuecenturion1@gmail.com>
2023-02-04 14:46:09 -0300

    news letter js, css and html

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
 import ProductListing from "./ProductList.mjs";
 import ProductData from "./ProductData.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
-
+import { newsLetterTemplate } from "./news_sign_up.mjs"
+import { responseToSubmission } from "./news_sign_up.mjs"
 loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const listing = new ProductListing("Tents", dataSource, element);
-
+/* Function that Renders the newsletter form and say "Thanks when submitted" */
+newsLetterTemplate()
+document.getElementById("submit-button").addEventListener("click", responseToSubmission);
 listing.init();
