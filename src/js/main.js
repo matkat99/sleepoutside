adcbebd757798f8036c1a5fcf8fd6ddfa1733a2e
Felix <fguzmanceri@gmail.com>
2023-02-04 18:03:30 -0400

    numberItems main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 import ProductListing from "./ProductList.mjs";
 import ProductData from "./ProductData.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, numberItems } from "./utils.mjs";
 import { newsLetterTemplate } from "./news_sign_up.mjs"
 import { responseToSubmission } from "./news_sign_up.mjs"
 loadHeaderFooter();
-
+numberItems("so-cart");
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const listing = new ProductListing("Tents", dataSource, element);
