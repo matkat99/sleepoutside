622bdf3d15d481f1fe9505d998e688bfd826c6fd
Felix <fguzmanceri@gmail.com>
2023-02-06 20:15:47 -0400

    saving main

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,7 +3,7 @@ import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
 numberItems("so-cart");
-getLocalStorage("so-cart", this.addToCart);
+getLocalStorage("so-cart");
 const ShoppingBag =  new ShoppingCart("so-cart", ".product-list");
 
 ShoppingBag.renderCartContents();
