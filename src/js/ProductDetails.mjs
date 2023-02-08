5874a7346c831a26f51f8f10c2fe807fbe93d3e5
Felix <fguzmanceri@gmail.com>
2023-02-08 13:33:23 -0400

    saving cart

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -41,15 +41,32 @@ export default class ProductDetails {
       .addEventListener("click", this.addToCart.bind(this));
   }
   addToCart() {
-    let cartContents = getLocalStorage("so-cart");
-    //check to see if there was anything there
-    if (!cartContents) {
-      cartContents = [];
+    document.querySelector(".rise-shake").style.animation = "jump-shaking 0.83s"
+
+    let Data = getLocalStorage("so-cart");
+    if (Data) {
+      let tent = 1;
+      for (let i = 0; i < Data.length; i++) {
+        if (Data[i].Id == this.product.Id) {
+          this.product.quantity = Data[i].quantity++;
+          tent = 0;
+        }
+      }
+
+      if (tent == 1) {
+        this.product.quantity = 1;
+        Data.push(this.product);
+      }
+      
+    } else {
+      Data = [];
+      this.product.quantity = 1;
+      Data.push(this.product);
     }
-    // then add the current product to the list
-    cartContents.push(this.product);
-    setLocalStorage("so-cart", cartContents);
+
+    setLocalStorage("so-cart", Data);
   }
+
   renderProductDetails(selector) {
     const element = document.querySelector(selector);
     element.insertAdjacentHTML(
