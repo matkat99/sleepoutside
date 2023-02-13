075ff55add739345fcd0adcd770361b082b21f60
Felix <fguzmanceri@gmail.com>
2023-02-13 11:42:02 -0400

    saving updates

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         cart: resolve(__dirname, "src/cart/index.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
         listing: resolve(__dirname, "src/product-listing/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
       },
     },
   },
