a3f8ada82bd6b5df4663529f8826392f3eabe7cb
Felix <fguzmanceri@gmail.com>
2023-02-13 11:46:41 -0400

    saving checkout update

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,6 +1,4 @@
-// const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
-const baseURL = "http://server-nodejs.cit.byui.edu:3000/checkout";
-
+const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -10,39 +8,21 @@ function convertToJson(res) {
 }
 
 export default class ExternalServices {
-  // constructor(category) {
-  //   // this.category = category;
-  //   // this.path = `../json/${this.category}.json`;
-   //}
-
-
+  constructor(category) {
+    // this.category = category;
+    // this.path = `../json/${this.category}.json`;
+  }
   async getData(category) {
-
-    // http://server-nodejs.cit.byui.edu:3000/products/search/tents
-
     const response = await fetch(baseURL + `products/search/${category}`);
     const data = await convertToJson(response);
     return data.Result;
-  
   }
-  // getData() {
-  //   return fetch(this.path)
-  //     .then(convertToJson)
-  //     .then((data) => data);
-  // }
-  
   async findProductById(id) {
-
-    // http://server-nodejs.cit.byui.edu:3000/product/989CH
-    
     const response = await fetch(baseURL + `product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
-
-    // const products = await this.getData();
-    // return products.find((item) => item.Id === id);
   }
-  async checkout(payload){
+  async checkout(payload) {
     const options = {
       method: "POST",
       headers: {
