23ab84cac5a26796551b5c77cb5385d4c1ce9a8a
Felix <fguzmanceri@gmail.com>
2023-02-14 19:58:24 -0400

    breadcrumbs

--- src/js/Breadcrumbs.mjs
+++ src/js/Breadcrumbs.mjs
@@ -1,24 +1,9 @@
 import { numberItems } from "./utils.mjs";
 const breadcrumb = document.querySelector(".breadcrumb");
-const path = window.location.pathname;
+const category = new URLSearchParams(window.location.search).get("category");
 
-if (path === "/") {
-  breadcrumb.style.display = "none"; // hide breadcrumb on home page
-} else if (path.startsWith("/product-category")) {
-  const category = path.split("/").pop();
-  const numItems = numberItems("so-cart"); // replace with actual count
-  breadcrumb.innerHTML = `
-    <li><a href="../index.html">Home</a></li>
-    <li><a href="../product-listing/index.html?category">Product Category</a></li>
-    <li>./product-listing/index.html?${category}=tents</li>
-    <li class="active">${numItems} items</li>
-  `;
-} else if (path.startsWith("/product/")) {
-  const category = "Product Category"; // replace with actual category
-  const product = path.split("/").pop();
-  breadcrumb.innerHTML = `
-    <li><a href="/">Home</a></li>
-    <li><a href="/product-category">${category}</a></li>
-    <li>${product}</li>
-  `;
+if (category) {
+  breadcrumb.innerHTML = `<a href="/">Home</a> > <a href="/product-listing/index.html">${category}</a> > (${numberItems} items)`;
+} else {
+  breadcrumb.innerHTML = `<a href="/">Home</a>`;
 }
