75543e980dbc33d28efed174dec1d8a67175f462
Felix <fguzmanceri@gmail.com>
2023-02-17 00:17:52 -0400

    updates

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -4,18 +4,25 @@ import { loadHeaderFooter, numberItems, getParam } from "./utils.mjs";
 import { logProductCard } from "./QuickLook.mjs";
 
 const category = getParam("category");
-
+const product = getParam("product");
 const dataSource = new ExternalServices();
 const element = document.querySelector(".product-list");
 const listing = new ProductListing(category, dataSource, element);
 
+// Create the breadcrumb navigation
+const breadcrumbElement = document.querySelector(".breadcrumb");
+let breadcrumbHtml = "";
 if (category) {
-  const breadcrumb = document.querySelector(".breadcrumb");
-  breadcrumb.innerHTML = `${category} &rarr; (${listing.products.length} items)`;
+  breadcrumbHtml += `<a href="/">Home</a> > <a href="/?category=${category}">${category}</a>`;
+  if (product) {
+    breadcrumbHtml += ` > ${product}`;
+  }
+} else {
+  breadcrumbHtml += "Home";
 }
+breadcrumbElement.innerHTML = breadcrumbHtml;
 
 logProductCard();
 loadHeaderFooter();
 numberItems("so-cart",".numberCartItems");
-
 listing.init();
