847b06405c1734323c78b5d5a1f59fe8f94ed120
Felix <fguzmanceri@gmail.com>
2023-02-17 19:15:26 -0400

    saving process

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -78,9 +78,9 @@ export default class CheckoutProcess {
         console.log("json", json);
         try {
             const res = await services.checkout(json);
-            console.log(res).ExternalServices;
+            console.log(res);
         } catch (err) {
-        console.log(err).ExternalServices;
+        console.log(err);
         }
     }
 }
