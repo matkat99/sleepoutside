9d058b6bcae16d949a29d56118da14eb0e1baf93
Felix <fguzmanceri@gmail.com>
2023-02-17 00:27:31 -0400

    update

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -3,14 +3,15 @@ import ExternalServices from "./ExternalServices.mjs";
 import { loadHeaderFooter, numberItems, getParam } from "./utils.mjs";
 import { logProductCard } from "./QuickLook.mjs";
 
+loadHeaderFooter();
+numberItems("so-cart", ".numberCartItems");
+
 const category = getParam("category");
+
 const dataSource = new ExternalServices();
 const element = document.querySelector(".product-list");
 const listing = new ProductListing(category, dataSource, element);
 
-
-
 logProductCard();
-loadHeaderFooter();
-numberItems("so-cart",".numberCartItems");
+
 listing.init();
