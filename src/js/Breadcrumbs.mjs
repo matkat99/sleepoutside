5a0dd5283f729a79901363a3640871c4c900c003
Felix <fguzmanceri@gmail.com>
2023-02-17 00:26:49 -0400

    updates

--- src/js/Breadcrumbs.mjs
+++ src/js/Breadcrumbs.mjs
@@ -1,59 +1,16 @@
-/* eslint-disable no-param-reassign */
-function generateBreadcrumb() {
-  // Get the current page URL
-  const currentPageUrl = window.location.href;
+import { getParam } from "./utils.mjs";
+const category = getParam("category");
+const product = getParam("product");
 
-  // Get the breadcrumb element
-  const breadcrumbElement = document.getElementById("breadcrumb");
-
-  // Create an empty array to hold the breadcrumb items
-  const breadcrumbItems = [];
-
-  // Add the Home item to the breadcrumb, unless we're on the home page
-  if (currentPageUrl !== "http://example.com/") {
-    breadcrumbItems.push(`<a href="./index.html">Home</a>`);
+// Create the breadcrumb navigation
+const breadcrumbElement = document.querySelector(".breadcrumb");
+let breadcrumbHtml = "";
+if (category) {
+  breadcrumbHtml += `<a href="/">Home</a> > <a href="/?category=${category}">${category}</a>`;
+  if (product) {
+    breadcrumbHtml += ` > ${product}`;
   }
-
-  // Add the product category item to the breadcrumb, if we're on a product list page
-  const productCategory = getParameterByName("category");
-  if (productCategory) {
-    breadcrumbItems.push(
-      `<a href="./product-listing/index.html?category">Product Category</a>`
-    );
-    breadcrumbItems.push(
-      `<a href="./product-listing/index.html?category=${productCategory}">${productCategory}</a>`
-    );
-  }
-
-  // Add the number of products to the product category item, if we're on a product list page
-  const numberOfProducts = getNumberOfProducts();
-  if (numberOfProducts !== undefined) {
-    const productCategoryItem = breadcrumbItems[breadcrumbItems.length - 1];
-    breadcrumbItems[breadcrumbItems.length - 1] = `${productCategoryItem} &rarr; (${numberOfProducts} items)`;
-  }
-
-  // Add the breadcrumb items to the breadcrumb element
-  breadcrumbElement.innerHTML = breadcrumbItems.join(" &rarr; ");
+} else {
+  breadcrumbHtml += "Home";
 }
-
-// Get the value of a query string parameter by name
-function getParameterByName(name, url = window.location.href) {
-  name = name.replace(/[\\[\]]/g, "\\$&");
-  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
-  const results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return "";
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-// Get the number of products on the current page
-function getNumberOfProducts() {
-  const numberOfProductsElement = document.querySelector(".number-of-products");
-  if (numberOfProductsElement) {
-    return numberOfProductsElement.textContent;
-  }
-  return undefined;
-}
-
-// Call the generateBreadcrumb function to generate the breadcrumb on page load
-generateBreadcrumb();
+breadcrumbElement.innerHTML = breadcrumbHtml;
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -4,23 +4,11 @@ import { loadHeaderFooter, numberItems, getParam } from "./utils.mjs";
 import { logProductCard } from "./QuickLook.mjs";
 
 const category = getParam("category");
-const product = getParam("product");
 const dataSource = new ExternalServices();
 const element = document.querySelector(".product-list");
 const listing = new ProductListing(category, dataSource, element);
 
-// Create the breadcrumb navigation
-const breadcrumbElement = document.querySelector(".breadcrumb");
-let breadcrumbHtml = "";
-if (category) {
-  breadcrumbHtml += `<a href="/">Home</a> > <a href="/?category=${category}">${category}</a>`;
-  if (product) {
-    breadcrumbHtml += ` > ${product}`;
-  }
-} else {
-  breadcrumbHtml += "Home";
-}
-breadcrumbElement.innerHTML = breadcrumbHtml;
+
 
 logProductCard();
 loadHeaderFooter();
