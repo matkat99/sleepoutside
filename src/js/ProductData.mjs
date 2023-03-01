d0f7c57e65dd885d4edd52d4040c2c8856ac5bd1
Shane T <st83440@gmail.com>
2023-03-01 16:10:58 -0700

    remove classes

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -6,18 +6,13 @@ function convertToJson(res) {
   }
 }
 
-export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
-  }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
-  }
-  async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
-  }
+export function getData(category = "tents") {
+  return fetch(`../json/${category}.json`)
+    .then(convertToJson)
+    .then((data) => data);
+}
+
+export async function findProductById(id) {
+  const products = await getData();
+  return products.find((item) => item.Id === id);
 }
--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,12 @@
 import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-
-const dataSource = new ProductData("tents");
+import { findProductById } from "./ProductData.mjs";
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
+  const product = await findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
