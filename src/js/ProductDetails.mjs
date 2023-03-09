be40fc6a190b9855df9f9cf9296ab9ba3fe47ea1
Shane T <st83440@gmail.com>
2023-03-09 15:27:03 -0700

    fix case issues

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,9 +1,9 @@
-import { findProductById } from "./ProductData.mjs";
+import { findProductById } from "./productData.mjs";
 import { setLocalStorage } from "./utils.mjs";
 
 let product = {};
 
-export default async function ProductDetails(productId) {
+export default async function productDetails(productId) {
   // get the details for the current product. findProductById will return a promise! use await or .then() to process it
   product = await findProductById(productId);
   // once we have the product details we can render out the HTML
