cdee910e756819a89bc73738532035f752fee356
Shane T <st83440@gmail.com>
2023-03-10 10:39:37 -0700

    add alert to add to cart

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,5 +1,5 @@
 import { findProductById } from "./externalServices.mjs";
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, alertMessage } from "./utils.mjs";
 
 let product = {};
 
@@ -20,6 +20,7 @@ function addToCart() {
   // then add the current product to the list
   cartContents.push(product);
   setLocalStorage("so-cart", cartContents);
+  alertMessage(`${product.NameWithoutBrand} added to cart!`);
 }
 function renderProductDetails() {
   document.querySelector("#productName").innerText = product.Brand.Name;
