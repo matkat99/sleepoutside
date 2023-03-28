8d3b0858c6c1914d8d6b53a9c2b1312c6035fbd3
Shane T <st83440@gmail.com>
2023-03-28 15:03:46 -0600

    initial 3.5

--- src/js/product.js
+++ src/js/product.js
@@ -1,16 +1,5 @@
-import { setLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productId = getParam("product");
+productDetails(productId, ".product-detail");
---
+++ src/js/productDetails.mjs
@@ -0,0 +1,35 @@
+import { findProductById } from "./productData.mjs";
+import { setLocalStorage } from "./utils.mjs";
+
+let product = {};
+
+export default async function productDetails(productId, selector) {
+  // get the details for the current product. findProductById will return a promise! use await or .then() to process it
+  product = await findProductById(productId);
+  // once we have the product details we can render out the HTML
+  const el = document.querySelector(selector);
+  el.insertAdjacentHTML("afterBegin", productDetailsTemplate(product));
+  // once the HTML is rendered we can add a listener to Add to Cart button
+  document.getElementById("addToCart").addEventListener("click", addToCart);
+}
+function addToCart() {
+  setLocalStorage("so-cart", product);
+}
+
+function productDetailsTemplate(product) {
+  return `<h3>${product.Brand.Name}</h3>
+  <h2 class="divider">${product.NameWithoutBrand}</h2>
+  <img
+    class="divider"
+    src="${product.Image}"
+    alt="${product.Name}"
+  />
+  <p class="product-card__price">$${product.FinalPrice}</p>
+  <p class="product__color">${product.Colors[0].ColorName}</p>
+  <p class="product__description">
+  ${product.DescriptionHtmlSimple}
+  </p>
+  <div class="product-detail__add">
+    <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+  </div>`;
+}
