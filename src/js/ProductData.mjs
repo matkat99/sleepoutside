8dd100e7ebd49cf3589a9abb0723dfd993629274
EzequielRindello <rindelloezequiel@gmail.com>
2024-01-26 20:52:26 -0300

    .

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -17,6 +17,7 @@ export default class ProductData {
   async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
     const data = await convertToJson(response);
+    console.log(data)
     return data.Result;
   }
   async findProductById(id) {
