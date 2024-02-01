5ef0adea83d3e546292343538f09bb4b8d13115a
EzequielRindello <rindelloezequiel@gmail.com>
2024-02-01 18:04:12 -0300

    vite.config.js update

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
         product: resolve(__dirname, "src/product_pages/index.html"),
         listing: resolve(__dirname, "src/product-listing/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
+        success: resolve(__dirname, "src/checkout/success.html")
       },
     },
   },
