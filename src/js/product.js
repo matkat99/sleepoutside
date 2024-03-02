0f0909d7b1e022a178014fa09247a907bcfc4e7f
drspahr <spahrdg@gmail.com>
2024-03-01 21:06:14 -0600

    Re-coded product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  let cart = getLocalStorage('so-cart') || [];
+  cart.push(product);
+  setLocalStorage('so-cart', cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
