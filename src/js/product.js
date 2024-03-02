4e5ee728bef582222795468977cbaaaaa1ab9c09
TuckTuck94 <114019356+TuckTuck94@users.noreply.github.com>
2024-03-01 21:45:14 -0600

    commit changes to individual

--- src/js/product.js
+++ src/js/product.js
@@ -4,6 +4,7 @@ import ProductData from './ProductData.mjs';
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
+  //cart array needed
   let cart = getLocalStorage('so-cart') || [];
   cart.push(product);
   setLocalStorage('so-cart', cart);
