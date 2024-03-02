abf9a4af85a25236bb196299f830a70f2dbaf41d
Krysta Hunt <kdhunt@musefish.net>
2024-03-01 20:24:07 -0700

    Update cart.js

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,9 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage('so-cart');
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.querySelector('.product-list').innerHTML = htmlItems.join('');
 }
 
 function cartItemTemplate(item) {
