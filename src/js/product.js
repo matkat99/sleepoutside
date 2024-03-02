2679b16e256016c534da9fbf91f7c6b14e10ab2f
Krysta Hunt <kdhunt@musefish.net>
2024-03-01 20:25:03 -0700

    Update product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,7 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
   let cart = getLocalStorage('so-cart') || [];
@@ -16,5 +16,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
