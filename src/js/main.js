89d38204b9965b76464f36a7d213dc25590e4f80
kdhunt <kdhunt@musefish.net>
2024-03-13 07:54:59 -0600

    alerts

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
+import Alert from './Alert';
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
 
+const alert = new Alert();
 const dataSource = new ProductData('tents');
 const element = document.querySelector('.product-list');
 const productList = new ProductList('Tents', dataSource, element);
+
 productList.init();
