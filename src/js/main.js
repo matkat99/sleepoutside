59acb61aef2967ed19ffbca455ef637565b309ef
kdhunt <kdhunt@musefish.net>
2024-03-14 19:06:29 -0600

    updated header and footer to partials for team assignment 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 import Alert from './Alert';
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
-import {showCountItemsInCart} from './utils.mjs';
+import {showCountItemsInCart, loadHeaderFooter} from './utils.mjs';
 
+await loadHeaderFooter();
 const alert = new Alert();
 const dataSource = new ProductData('tents');
 const element = document.querySelector('.product-list');
