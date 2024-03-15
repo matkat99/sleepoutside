a0b8ec3ac3098c3cd527a2d0d56fb99972a383a6
kdhunt <kdhunt@musefish.net>
2024-03-14 19:16:59 -0600

    lint fixes

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
 import {showCountItemsInCart, loadHeaderFooter} from './utils.mjs';
 
-await loadHeaderFooter();
+loadHeaderFooter();
 const alert = new Alert();
 const dataSource = new ProductData('tents');
 const element = document.querySelector('.product-list');
