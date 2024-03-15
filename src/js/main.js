18db95c5ccbdc57216b2bbef47f7f5171378fcf3
kdhunt <kdhunt@musefish.net>
2024-03-14 19:59:57 -0600

    bug fixes

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ loadHeaderFooter();
 const alert = new Alert();
 const dataSource = new ProductData('tents');
 const element = document.querySelector('.product-list');
-const productList = new ProductList('Tents', dataSource, element);
+const productList = new ProductList('tents', dataSource, element);
 
 productList.init();
 
