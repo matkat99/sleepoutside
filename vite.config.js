7ded1eb7bd8b0398b888687d6cdfba7f74afcf93
Jonathan de Almeida <47408354+jonathanhost@users.noreply.github.com>
2024-06-14 01:24:23 -0300

    new comit

--- vite.config.js
+++ vite.config.js
@@ -13,16 +13,16 @@ export default defineConfig({
         checkout: resolve(__dirname, "src/checkout/index.html"),
         product1: resolve(
           __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html"
+          "src/product_pages/cedar-ridge-rimrock-2.html",
         ),
         product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
         product3: resolve(
           __dirname,
-          "src/product_pages/northface-alpine-3.html"
+          "src/product_pages/northface-alpine-3.html",
         ),
         product4: resolve(
           __dirname,
-          "src/product_pages/northface-talus-4.html"
+          "src/product_pages/northface-talus-4.html",
         ),
       },
     },
