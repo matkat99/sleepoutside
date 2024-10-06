94c9bdf9d427d14bef9fc7d9ba31befc0066dde8
Mickael <micrandr@gmail.com>
2024-10-06 05:20:26 +0300

    feat: add product dynamically, header, footer

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -6,18 +6,31 @@ function convertToJson(res) {
   }
 }
 
-export default class ProductData {
+export async function findProductById(id) {
+  const products = await getData();
+  return products.find((item) => item.Id === id);
+} 
+
+export function getData(category = "tents") {
+  return fetch(`../json/${category}.json`)
+    .then(convertToJson)
+    .then((data) => data);
+}
+
+
+export default class ProductData  {
   constructor(category) {
     this.category = category;
     this.path = `../json/${this.category}.json`;
   }
-  getData() {
+  getData() {    
     return fetch(this.path)
+      //.then(convertToJson).then((data) => data);
       .then(convertToJson)
-      .then((data) => data);
+      .then((data) => data.filter((item) => item.Id != '989CG' && item.Id != '880RT'));
   }
   async findProductById(id) {
-    const products = await this.getData();
+    const products = await this.getData()
     return products.find((item) => item.Id === id);
   }
 }
