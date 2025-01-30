26f3e47a25afed4eda5baa263efac3756bdeb680
Shane T <st83440@gmail.com>
2025-01-30 16:18:24 -0700

    update for svelte5, remove store and use shared state rune

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
+import { mount } from "svelte";
 import ProductList from "./components/ProductList.svelte";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-new ProductList({
+const productList = mount(ProductList, {
   target: document.querySelector(".products"),
   props: { category: "tents" },
 });
